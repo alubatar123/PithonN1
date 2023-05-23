1d44d687cc012b562223b4ee980ab7c5bd9b829e
alubatar123 <alubatar123@gmail.com>
2023-05-22 23:05:05 -0600

    BlackJack1

---
+++ BlackJack/Juego.py
@@ -0,0 +1,23 @@
+from Mazo import Baraja
+import random 
+
+MiMazo= Baraja()
+BarajaCompleta=MiMazo.CreaMazo()
+MazoTemp=[]
+
+def BarajaCartas():    
+    random.shuffle(BarajaCompleta)
+    
+def TiraCartas():
+    MazoTemp.append(BarajaCompleta.pop(0))    
+    MiMazo.CartaAspecto(*MazoTemp)
+    Select=input("¿Desea otra carta?Y/N ")
+    if Select == "y" or Select == "Y":
+        TiraCartas()
+
+
+
+BarajaCartas()
+print(BarajaCompleta)
+TiraCartas()
+print(BarajaCompleta)
