74d694ad5f173ec8dbab30d7d4e9874263a0fb8d
alubatar123 <alubatar123@gmail.com>
2023-07-01 20:03:08 -0600

    DB+Decorator

--- MyBDApp/CRUD.py
+++ MyBDApp/CRUD.py
@@ -5,14 +5,27 @@ import os
 root=Tk()
 barraMenu=Menu(root)
 root.config(menu=barraMenu,width=300, height=300)
-
+miCursor=""
 
 #-----------------------------Funciones-----------------------------
 
+def Abre_Cierra_DB(funcion_parametro):
+    
+    def funcion_interna():
+        global miCursor
+        miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")        
+        miCursor=miConexion.cursor()
+        funcion_parametro()
+        miConexion.commit()
+        miConexion.close()
+        
+    return funcion_interna
+
+@Abre_Cierra_DB
 def conexionBBDD():
     
-    miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
-    miCursor=miConexion.cursor()
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
     try:
         miCursor.execute('''
                 CREATE TABLE DATAUSUARIOS(
@@ -26,7 +39,7 @@ def conexionBBDD():
         messagebox.showinfo("BBDD","BBDD creada")
     except:
         messagebox.showwarning("ERROR,", "La BBDD ya existe")
-    miConexion.close()
+    #miConexion.close()
 
 
 
@@ -55,9 +68,10 @@ def limpiarCampos():
     miPass.set("")
     textoComentario.delete(1.0,END)#para texto se usa delete. 1.0= punto de partida
 
+@Abre_Cierra_DB
 def Crear():
-    miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
-    miCursor=miConexion.cursor()
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
     #Usamos NULL xq es autonumerico
     miCursor.execute("INSERT INTO DATAUSUARIOS VALUES(NULL,'"
                      +miNombre.get()+"','"
@@ -65,9 +79,75 @@ def Crear():
                      +miApellido.get()+"','"
                      +miDireccion.get()+"','"
                      +textoComentario.get("1.0",END)+"')")
-    miConexion.commit()
+    
+
+    #miConexion.commit()
     messagebox.showinfo("BBDD","Registro Asignaod")
-    miConexion.close()
+    limpiarCampos()
+    #miConexion.close()
+
+@Abre_Cierra_DB   
+def CrearOption2():
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
+    
+    datos=miNombre.get(),miPass.get(),miApellido.get(),miDireccion.get(),textoComentario.get("1.0",END)
+    miCursor.execute("INSERT INTO DATAUSUARIOS VALUES(NULL,?,?,?,?,?)",datos)
+    #miConexion.commit()
+    messagebox.showinfo("BBDD","Registro Asignaod")
+    limpiarCampos()
+   # miConexion.close()
+@Abre_Cierra_DB   
+def Leer():
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
+    miCursor.execute("SELECT * FROM DATAUSUARIOS WHERE ID="+miID.get())
+    ElUsuario=miCursor.fetchall()
+    for e in ElUsuario:
+        miID.set(e[0])
+        miNombre.set(e[1])
+        miPass.set(e[2])
+        miApellido.set(e[3])
+        miDireccion.set(e[4])
+        textoComentario.insert(1.0,e[5])
+    #miConexion.commit()
+    #miConexion.close()    
+@Abre_Cierra_DB   
+def ActualizarOpion2():
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
+    datos=miNombre.get(),miPass.get(),miApellido.get(),miDireccion.get(),textoComentario.get("1.0",END)
+    miCursor.execute("UPDATE DATAUSUARIOS SET NOMBRE_USUARIO=?,PASSWORD=?,APELLIDO=?,DIRECCION=?,COMMENTARIOS=?"+
+                     "WHERE ID="+miID.get(),datos)
+    #miConexion.commit()
+    messagebox.showinfo("Actualizacion","Campo actualizado")
+    limpiarCampos()
+    #miConexion.close()   
+@Abre_Cierra_DB   
+def Actualizar():
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
+    miCursor.execute("UPDATE DATAUSUARIOS SET "+
+                     " NOMBRE_USUARIO='"+miNombre.get()+"',"
+                     " PASSWORD='"+miPass.get()+"',"
+                     " APELLIDO='"+miApellido.get()+"',"
+                     " DIRECCION='"+miDireccion.get()+"',"
+                     " COMMENTARIOS='"+textoComentario.get("1.0",END)+"'"
+                     " WHERE ID="+miID.get())
+    #miConexion.commit()
+    messagebox.showinfo("Actualizacion","Campo actualizado")
+    limpiarCampos()
+    #miConexion.close()  
+@Abre_Cierra_DB   
+def eliminar():
+    #miConexion=sqlite3.connect("Curso\PithonN1\MyBDApp\MiUsuarios.sqlite3")
+    #miCursor=miConexion.cursor()
+    temp=miID.get()
+    miCursor.execute("DELETE FROM DATAUSUARIOS WHERE ID="+miID.get())
+    #miConexion.commit()
+    #miConexion.close()  
+    messagebox.showinfo("BBDD", f"Usuario{temp} ha sido borrado")
+    limpiarCampos()
 
 #-----------------------------Barra menu-----------------------------
 BDMenu=Menu(barraMenu,tearoff=0)
@@ -80,9 +160,9 @@ borrarMenu.add_command(label="Borrar campos",command=limpiarCampos)
 
 crudMenu=Menu(barraMenu,tearoff=0)
 crudMenu.add_command(label="Crear",command=Crear)
-crudMenu.add_command(label="Leer")
-crudMenu.add_command(label="Actualizar")
-crudMenu.add_command(label="Borrar")
+crudMenu.add_command(label="Leer",command=Leer)
+crudMenu.add_command(label="Actualizar",command=Actualizar)
+crudMenu.add_command(label="Borrar",command=eliminar)
 
 ayudaMenu=Menu(barraMenu,tearoff=0)
 ayudaMenu.add_command(label="Licensia")
@@ -153,16 +233,16 @@ CommentLabel.grid(row=5,column=0,sticky="e",padx=10,pady=10)
 miFrame2=Frame(root)
 miFrame2.pack()
 
-botonCrear=Button(miFrame2,text="Create",command=Crear)
+botonCrear=Button(miFrame2,text="Create",command=CrearOption2)
 botonCrear.grid(row=1,column=0,sticky="e",padx=10,pady=10)
 
-botonLeer=Button(miFrame2,text="Leer")
+botonLeer=Button(miFrame2,text="Leer",command=Leer)
 botonLeer.grid(row=1,column=1,sticky="e",padx=10,pady=10)
 
-botonActualizar=Button(miFrame2,text="Actualizar")
+botonActualizar=Button(miFrame2,text="Actualizar",command=ActualizarOpion2)
 botonActualizar.grid(row=1,column=2,sticky="e",padx=10,pady=10)
 
-botonBorrar=Button(miFrame2,text="Borrar")
+botonBorrar=Button(miFrame2,text="Borrar",command=eliminar)
 botonBorrar.grid(row=1,column=3,sticky="e",padx=10,pady=10)
 
 
